0e86a76ba80384638b89f77962a740d11928a2e4
Sergio Ramos <sergioramosdavid@hotmail.com>
2023-10-04 17:44:32 -0400

    prueba commit directo

--- pages/usuarios/nuevo.js
+++ pages/usuarios/nuevo.js
@@ -7,6 +7,7 @@ import UserContext from '../../components/UserContext'
 import GetImg from '../../components/GetImg'
 import FormData from 'form-data'
 import Notifications, { notify } from 'react-notify-toast'
+// Prueba commit directo
 const nuevoUsuario = () => {
   const { signOut, getSucursales } = useContext(UserContext)
   const [token, setToken] = useState(false)
